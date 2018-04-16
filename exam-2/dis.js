a5bbbc0ff5e0b1e297ac9bc829d4715adc4cc86c
Hunter <charvat8@gmail.com>
2018-04-16 14:25:17 -0400

    through the js part
    got the javascript done

---
+++ exam-2/dis.js
@@ -0,0 +1,8 @@
+function dis() {
+    var x = document.getElementById("left-pic");
+    if (x.style.display === "none") {
+        x.style.display = "inline";
+    } else {
+        x.style.display = "none";
+    }
+} 
